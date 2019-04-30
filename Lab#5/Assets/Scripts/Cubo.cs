230e126e282fa47366da63047a9eeed3dbee73eb
Cristina B <bau161260@uvg.edu.gt>
2019-04-29 18:12:05 -0600

    Use el lab anterior y le cambie los materiales a la escena

---
+++ Lab#5/Assets/Scripts/Cubo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour
+{
+    public Renderer rend;
+    private Rigidbody rb;
+    public float force = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddForce(Input.GetAxis("Horizontal") * force, 0, Input.GetAxis("Vertical") * force);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(myRay, out hitInfo))
+            rb.AddForce(-hitInfo.normal * force, ForceMode.Impulse);
+    }
+
+    public void OnMouseOver()
+    {
+        rend.material.color = Color.magenta;
+    }
+
+    public void OnMouseExit()
+    {
+        rend.material.color = Color.green;
+    }
+}
---
+++ Lab#5/Assets/Scripts/Personita.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Personita : MonoBehaviour
+{
+    public int puntuacion = 0;
+    public Text TxtContador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Input.GetMouseButtonDown(0))
+            if(Physics.Raycast(myRay, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.CompareTag("Blancos"))
+                {
+                    Destroy(hitInfo.collider.gameObject);
+                    puntuacion += 1;
+                    TxtContador.text = puntuacion.ToString();
+                }
+            }
+    }
+}
